a43d2a91bf1116ffa0daaf25e911c515c9f377b0
Vivian Kong <vivkong@ca.ibm.com>
2009-07-14 21:26:25 +0000

    bug 279855 - Unable to open include browser from Outline View

--- rdt/org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/actions/OpenViewActionGroup.java
+++ rdt/org.eclipse.ptp.rdt.ui/src/org/eclipse/ptp/internal/rdt/ui/actions/OpenViewActionGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -315,6 +315,7 @@ public class OpenViewActionGroup extends ActionGroup {
 	}
 	
 	public void setEnableIncludeBrowser(boolean enableIncludeBrowser) {
-		fEnableIncludeBrowser= enableIncludeBrowser;
+		fEnableIncludeBrowser = false; //remote include browser is only in PTP 3.0+
+//		fEnableIncludeBrowser= enableIncludeBrowser;
 	}
 }
