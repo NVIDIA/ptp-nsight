4b38fd143df54bddf7ae75a4e1025534355dcccb
Chris Recoskie <recoskie@ca.ibm.com>
2010-11-30 02:33:00 +0000

    Bug 331380 - process builders should clone environment from connection, not modify it

--- core/org.eclipse.ptp.remote.remotetools.core/src/org/eclipse/ptp/remote/remotetools/core/RemoteToolsConnection.java
+++ core/org.eclipse.ptp.remote.remotetools.core/src/org/eclipse/ptp/remote/remotetools/core/RemoteToolsConnection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.ptp.remote.remotetools.core;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -302,7 +303,7 @@ public class RemoteToolsConnection implements IRemoteConnection {
 				}
 			}
 		}
-		return fEnv;
+		return Collections.unmodifiableMap(fEnv);
 	}
 
 	/*
