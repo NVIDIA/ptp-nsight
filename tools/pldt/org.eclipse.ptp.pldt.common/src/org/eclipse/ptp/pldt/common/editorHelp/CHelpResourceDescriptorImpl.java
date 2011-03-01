ab3f2014307cfac8dc4aaddbc683d64a44494e95
Beth Tibbitts <tibbitts@us.ibm.com>
2011-03-01 21:33:47 +0000

    recognize alt html dir for fragment

--- tools/pldt/org.eclipse.ptp.pldt.common/src/org/eclipse/ptp/pldt/common/editorHelp/CHelpResourceDescriptorImpl.java
+++ tools/pldt/org.eclipse.ptp.pldt.common/src/org/eclipse/ptp/pldt/common/editorHelp/CHelpResourceDescriptorImpl.java
@@ -11,10 +11,16 @@
 
 package org.eclipse.ptp.pldt.common.editorHelp;
 
+import java.net.URL;
+
 import org.eclipse.cdt.ui.ICHelpBook;
 import org.eclipse.cdt.ui.ICHelpResourceDescriptor;
 import org.eclipse.cdt.ui.IFunctionSummary;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.help.IHelpResource;
+import org.osgi.framework.Bundle;
 
 public class CHelpResourceDescriptorImpl implements ICHelpResourceDescriptor
 {
@@ -36,8 +42,12 @@ public class CHelpResourceDescriptorImpl implements ICHelpResourceDescriptor
         name = functionSummary.getName();
         //href = "/"+pluginId + "/html/" + name + ".html";
         StringBuffer buf=new StringBuffer();
-        buf.append("/").append(pluginId).append("/html/").append(name).append(".html"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        href=buf.toString();
+        
+		// Find where the html dir is located - could vary e.g. if a fragment
+		// provides "extra" info
+        String htmlLocn=findHTMLdir(pluginId);
+		buf.append("/").append(pluginId).append("/").append(htmlLocn).append("/").append(name).append(".html"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		href = buf.toString();
         //System.out.println("looking for help file: "+href);
         // href="file://c:/temp/foo.html";      
         label = functionSummary.getPrototype().getPrototypeString(false);
@@ -66,5 +76,26 @@ public class CHelpResourceDescriptorImpl implements ICHelpResourceDescriptor
     }
     public String toString() {
     	return name+" -> "+href; //$NON-NLS-1$
+    }
+
+	/**
+	 * Workaraound to be able to recognize alternate html dir e.g. in a fragment
+	 * "add-on" plugin, to replace the html help files. 5.0 version will use an
+	 * extension point.
+	 * 
+	 * @param pluginId
+	 * @return
+	 */
+	private String findHTMLdir(String pluginId) {
+		String HTML_ALT_DIR = "html2";
+		// System.out.println("Looking or HTML dir for "+pluginId);
+    	Bundle bundle = Platform.getBundle(pluginId);
+		URL url = FileLocator.find(bundle, new Path(HTML_ALT_DIR), null);
+		if (url != null) {
+			return HTML_ALT_DIR;
+		} else {
+			return "html";
+		}
+
     }
 }
