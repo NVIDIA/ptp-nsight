49449538c04c5be35d73b1028255fd03ea11e2be
John Liu <jwsliu@ca.ibm.com>
2011-06-19 19:43:11 +0000

    fix 394712

--- rdt/org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/internal/rdt/core/IRemoteIndexerInfoProvider.java
+++ rdt/org.eclipse.ptp.rdt.core/src/org/eclipse/ptp/internal/rdt/core/IRemoteIndexerInfoProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 IBM Corporation and others.
+ * Copyright (c) 2008, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,7 +41,7 @@ public interface IRemoteIndexerInfoProvider extends IStandaloneScannerInfoProvid
 			KEY_SKIP_ALL_REFERENCES = "skipReferences", //$NON-NLS-1$
 			//KEY_SKIP_IMPLICIT_REFERENCES = "skipImplicitReferences", //$NON-NLS-1$
 			KEY_SKIP_TYPE_REFERENCES = "skipTypeReferences", //$NON-NLS-1$
-			KEY_SKIP_MACRO_REFERENCES = "updatePolicy"; //$NON-NLS-1$
+			KEY_SKIP_MACRO_REFERENCES = "skipMacroReferences"; //$NON-NLS-1$
 
 	/**
 	 * Returns true if the given indexer preference is enabled.
